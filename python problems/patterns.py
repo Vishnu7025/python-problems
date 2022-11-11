ce371b3a3f995b1d547c8286015e8c116483a8ec
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-11-11 12:05:38 +0530

    Intial commit

---
+++ python problems/patterns.py
@@ -0,0 +1,221 @@
+# block pattern
+# # # # # 
+# # # # # 
+# # # # #
+# # # # #
+# # # # #
+
+# for i in range(5):
+#     for k in range(5):
+#         print('#',end=' ')
+#     print('')
+
+
+# 
+# # 
+# # #
+# # # #
+# # # # #
+
+# for i in range(5):
+#     for k in range(i+1):
+#         print('#',end=' ')
+#     print('')
+
+    # # # # # 
+    # # # # 
+    # # #
+    # #
+    #
+# for i in range(5):
+#     for k in range(i,5):
+#         print('#',end=' ')  
+#     print('')
+
+
+          # 
+        # # 
+      # # #
+    # # # #
+  # # # # #
+# n=5
+# for i in range(n):
+#     for k in range(i,n):
+#         print(' ',end=' ')
+#     for k in range(i+1):
+#         print('#',end=' ')
+#     print()
+
+
+  # # # # # 
+    # # # # 
+      # # #
+        # #
+          #
+# n = 5
+# for i in range(n):
+#     for k in range(i+1):
+#         print(' ', end=' ')
+#     for k in range(i,n):
+#         print('#',end=' ')
+#     print()
+
+
+
+
+          # 
+        # # # 
+      # # # # #
+    # # # # # # #
+  # # # # # # # # #
+
+# n = 5
+# for i in range(n):
+#     for k in range(i,n):
+#         print(' ',end=' ')
+#     for k in range(i):
+#         print('#',end=' ')
+#     for k in range(i+1):
+#         print('#',end=' ')
+#     print()
+
+
+  # # # # # # # # # 
+    # # # # # # # 
+      # # # # #
+        # # #
+          #
+# n=5
+# for i in range(n):
+#     for k in range(i+1):
+#         print(' ',end=' ')
+#     for k in range(i,n-1):
+#         print('#',end=' ')
+#     for k in range(i,n):
+#         print('#',end=' ')
+#     print()
+
+
+          # 
+        # # # 
+      # # # # #
+    # # # # # # #
+  # # # # # # # # #
+    # # # # # # #
+      # # # # #
+        # # #
+          #
+
+# n = 5
+# for i in range(n-1):
+#     for k in range(i,n):
+#         print(' ',end=' ')
+#     for k in range(i):
+#         print('#',end=' ')
+#     for k in range(i+1):
+#         print('#',end=' ')
+#     print()
+# n=5
+# for i in range(n):
+#     for k in range(i+1):
+#         print(' ',end=' ')
+#     for k in range(i,n-1):
+#         print('#',end=' ')
+#     for k in range(i,n):
+#         print('#',end=' ')
+#     print()
+
+# *                 * 
+# * *             * * 
+# * * *         * * *
+# * * * *     * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * *     * * * *
+# * * *         * * *   
+# * *             * *
+# *                 *
+
+
+# n=5
+# for i in range(n):
+#     for k in range(i+1):
+#         print('*',end=' ')
+#     for k in range(i,n-1):
+#         print(' ',end=' ')
+#     for k in range(i,n-1):
+#         print(' ',end=' ')
+#     for k in range(i+1):
+#         print('*',end=' ')
+#     print()
+# for i in range(n):
+#     for k in range(i,n):
+#         print('*',end=' ')
+#     for k in range(i):
+#         print(' ',end=' ')
+#     for k in range(i):
+#         print(' ',end=' ')
+#     for k in range(i,n):
+#         print('*',end=' ')
+#     print()
+
+     # 
+    # # 
+   # # #
+  # # # #
+ # # # # #
+
+# n=5
+# for i in range(n):
+#     for k in range(i,n):
+#         #no need of ''
+#         print(end=' ')
+#     for k in range(i+1):
+#         print('#',end=' ')
+#     print()
+
+ # # # # # 
+  # # # # 
+   # # #
+    # #
+     #
+# n = 5
+# for i in range(n):
+#     for k in range(i+1):
+#         #no need ''
+#         print( end=' ')
+#     for k in range(i,n):
+#         print('#', end=' ')
+#     print()
+
+
+ # # # # # 
+  # # # # 
+   # # #
+    # #
+     #
+     #
+    # #
+   # # #
+  # # # #
+ # # # # #
+
+# n = 5
+# for i in range(n):
+#     for k in range(i+1):
+#         #no need ''
+#         print( end=' ')
+#     for k in range(i,n):
+#         print('#', end=' ')
+#     print()
+# n=5
+# for i in range(n):
+#     for k in range(i,n):
+#         #no need of ''
+#         print(end=' ')
+#     for k in range(i+1):
+#         print('#',end=' ')
+#     print()
+
+  
+
--- python problems/prime or not .py
+++ python problems/prime or not .py
@@ -1,27 +1,25 @@
-# num = int(input("Enter a positive number to check wheather it is prime or not:  "))
-# if num > 1:
-#     for i in range(2,num):
-#         if (num % i)== 0:
-#             print(num,"is not a prime number")
-#             break
-#         else:
-#             print(num,"is a prime number")
-#             break
-# else:
-#     print(num,"is not a prme number")
+num = int(input("Enter a positive number to check wheather it is prime or not:"))
+if num > 1:
+        for i in range(2,num):
+            if (num % i)== 0:
+                print(num,"is not a prime number")
+                break
+            else:
+                print(num,"is a prime number")
+                break
+            
+else:
+    print(num,"is not a prme number")
 
 
 #finding prime numbers between given two numbers
 
-for num in range(100,200):
-    for i in range(2,num):
-        if (num % i) == 0:
-            break
-    else:
-        print(num,end=' ')
-        
-
-
+# for num in range(2,50):
+#     for i in range(2,num):
+#         if (num % i) == 0:
+#             break
+#     else:
+#         print(num,end=' ')
 
 
 
